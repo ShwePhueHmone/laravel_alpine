86495a3372b853f3c4df18a269bf898369a6198f
SHWEPHUEHMONE-P\ShwePhueHmone <scm.shwephuehmone@gmail.com>
2022-11-01 10:35:17 +0630

    company crud

---
+++ alpine_laravel/app/Models/Company.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Company extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'image'
+    ];
+}
